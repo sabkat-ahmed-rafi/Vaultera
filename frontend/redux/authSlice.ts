0ac928c458b795f16c684487bff94474c71054b4
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-05 04:17:33 +0600

    feat: add the extra reducers

--- frontend/redux/authSlice.ts
+++ frontend/redux/authSlice.ts
@@ -1,10 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createUser, loginUser, logout, updateUser } from "./authThunks";
 
 
 interface User {
   uid: string;
-  name: string;
-  email: string;
+  name: string | null;
+  email: string | null;
 }
 
 interface AuthState {
@@ -29,6 +30,63 @@ const authSlice = createSlice({
             state.loading = action.payload;
         }
     },
+      extraReducers: (builder) => {
+    builder
+      // Create User
+      .addCase(createUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        if (action.payload) {
+          const { uid, email, displayName } = action.payload;
+          state.user = { uid, email, name: displayName };
+        }
+        state.loading = false;
+      })
+      .addCase(createUser.rejected, (state) => {
+        state.loading = false;
+      })
+
+      // Login User
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        if (action.payload) {
+          const { uid, email, displayName } = action.payload;
+          state.user = { uid, email, name: displayName };
+        }
+        state.loading = false;
+      })
+      .addCase(loginUser.rejected, (state) => {
+        state.loading = false;
+      })
+
+      // Update User
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        const { uid, email, displayName } = action.payload;
+        state.user = { uid, email, name: displayName };
+        state.loading = false;
+      })
+      .addCase(updateUser.rejected, (state) => {
+        state.loading = false;
+      })
+
+      // Logout
+      .addCase(logout.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+        state.loading = false;
+      })
+      .addCase(logout.rejected, (state) => {
+        state.loading = false;
+      });
+  },
 });
 
 export const { setUser, setLoading } = authSlice.actions;
