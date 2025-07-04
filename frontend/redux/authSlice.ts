7409cd7c15f3ff65286599f47d162045b8e310a0
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-05 02:35:54 +0600

    feat: create auth slice

---
+++ frontend/redux/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface AuthState {
+  user: User | null;
+  loading: boolean;
+}
+
+const initialState : AuthState = {
+  user: null,
+  loading: false,
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser: ( state, action: PayloadAction<User | null> ) => {
+            state.user = action.payload;
+        },
+        setLoading: ( state, action: PayloadAction<boolean> ) => {
+            state.loading = action.payload;
+        }
+    },
+});
+
+export const { setUser, setLoading } = authSlice.actions;
+export default authSlice.reducer;
