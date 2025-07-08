389957cc65560917283a657b6a5548e901f6e035
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-09 03:25:35 +0600

    feat: hash and compare user's auth password

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,7 @@
-// import { RequestHandler } from 'express';
+import bcrypt from 'bcrypt';
 import { Request, Response, NextFunction } from 'express';
 import { getUserByEmail, addUser } from '../service/userService';
+import { hashPassword } from '../utils/hashPassword';
 
 
 
@@ -13,6 +14,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       res.status(500).json( "User already exists" );
       return;
     }
+
+    const hashedPassword = hashPassword(userData.password);
+    userData.password = hashedPassword;
 
     const newUser = await addUser(userData);
     res.status(201).json({ message: "User created successfully", newUser });
@@ -27,8 +31,13 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userData = req.body;
     const user = await getUserByEmail({ email: userData.email });
+    const isRealUser = await bcrypt.compare(userData.password, user!.password);
     if (!user) {
       res.status(500).json( "User not found" );
+      return;
+    } 
+    if(!isRealUser) {
+      res.status(500).json(" Invalid credentials ");
       return;
     }
     res.status(201).json({ message: "User found", user });
