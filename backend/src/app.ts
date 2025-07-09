210f8fb0d9727ea82fb89e38731870f4e391e8cc
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-09 18:05:53 +0600

    feat: add the cookie-parser middleware and install its type

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import { config } from './config/config';
 import userRoutes from './routes/userRoutes';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
 
 // Middlewares 
+app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
     origin: config.frontend,
+    credentials: true,
 }))
 
 // Routes
