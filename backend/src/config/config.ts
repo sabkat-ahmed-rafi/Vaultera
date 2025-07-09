a5a54e61fcef98d68718a8720f5652f48c0451ad
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-09 16:10:24 +0600

    feat: add jwt secret in environment variable

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const config = {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5000,
     frontend: process.env.FRONTEND,
+    jwt_secret: process.env.JWT_SECRET, 
 };
