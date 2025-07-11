8782070a1a3af0789b1710a90187e84195e7cd0e
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-12 00:26:32 +0600

    feat: wrap the app with authLayer component

--- frontend/components/others/AuthLayer.tsx
+++ frontend/components/others/AuthLayer.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+'use client'
+
+import { checkAuthSession } from '@/redux/authThunks';
+import { useAppDispatch } from '@/redux/hooks'
+import React, { useEffect } from 'react'
 
 const AuthLayer = ( { children }: Readonly<{ children: React.ReactNode }> ) => {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuthSession());
+  }, [dispatch])
+
   return (
     <>{ children }</>
   )
