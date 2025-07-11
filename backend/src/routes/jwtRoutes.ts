a855646761a081f50982397f1fb0f29d2f9d0569
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-11 17:25:36 +0600

    feat: route created to check session

--- backend/src/routes/jwtRoutes.ts
+++ backend/src/routes/jwtRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { removeJwt, setJwt } from "../controllers/jwtController";
+import { checkSession, removeJwt, setJwt } from "../controllers/jwtController";
+import { verifyToken } from "../middleware/verifyToken";
 
 const router = Router();
 
 router.post('/set-jwt', setJwt);
 router.post('/remove-jwt', removeJwt);
+router.get('/session', verifyToken, checkSession)
 
 
 export default router;
