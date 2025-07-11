46ac2d24fb1b0bc0257f8ec8544d5c96234a7f24
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-11 23:39:12 +0600

    refactor: set withCredentials: true while setting jwt

--- frontend/utils/setJwt.ts
+++ frontend/utils/setJwt.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const setTokenInCookies = async (user: JwtUser) => {
     try{
-        await axios.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/set-jwt`, user);
+        await axios.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/set-jwt`, user, { withCredentials: true });
     } catch(error) {
         throw new Error("Failed to set JWT cookie");
     }
