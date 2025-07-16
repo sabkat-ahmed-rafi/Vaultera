b603807470820f5c2c076a0e9055fb58212701a5
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-16 16:34:23 +0600

    feat: create helper function to encode argon related fields

---
+++ frontend/utils/encoding.ts
@@ -0,0 +1,7 @@
+export function uint8ArrayToBase64(arr: Uint8Array): string {
+  return btoa(String.fromCharCode.apply(null, Array.from(arr)));
+}
+
+export function base64ToUint8Array(base64: string): Uint8Array {
+  return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+}
