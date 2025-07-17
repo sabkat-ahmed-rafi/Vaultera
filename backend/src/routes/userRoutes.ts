96ad66022aa4f4bd2d26e086bf9b553d02e28c95
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-17 14:36:20 +0600

    feat: create route to get user's vault key info

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, loginUser } from "../controllers/userController";
+import { checkUserVaultKeyInfo, createUser, loginUser } from "../controllers/userController";
 
 const router = Router();
 
 router.post('/sign-up', createUser);
 router.post('/sign-in', loginUser);
+router.get('/users/:email/vault-key-info', checkUserVaultKeyInfo);
 
 
 export default router;
