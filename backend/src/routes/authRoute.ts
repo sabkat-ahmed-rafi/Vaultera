f052d49fce79f7e03ec073051689c7baa49cbc43
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-17 15:00:07 +0600

    refactor: merge the jwt and auth related route in a same file

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
+import { checkUserVaultKeyInfo, createUser, loginUser } from "../controllers/userController";
 import { checkSession, removeJwt, setJwt } from "../controllers/jwtController";
 import { verifyToken } from "../middleware/verifyToken";
 
 const router = Router();
 
+// User Authentication
+router.post('/auth/sign-up', createUser);
+router.post('/auth/sign-in', loginUser);
+router.get('/auth/users/:email/vault-key-info', checkUserVaultKeyInfo);
+
+// JWT Session Handling
 router.post('/auth/set-jwt', setJwt);
 router.post('/auth/remove-jwt', removeJwt);
 router.get('/auth/session', verifyToken, checkSession);
-
 
 export default router;
