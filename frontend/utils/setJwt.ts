0d61709fb5150dfdd6912f43becd41953694a043
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-19 00:36:50 +0600

    refactor: update jwt url with the new route

--- frontend/utils/setJwt.ts
+++ frontend/utils/setJwt.ts
@@ -1,9 +1,10 @@
+import { config } from "@/config/config";
 import { JwtUser } from "@/types/JwtUser";
 import axios from "axios";
 
 export const setTokenInCookies = async (user: JwtUser) => {
     try{
-        await axios.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/set-jwt`, user, { withCredentials: true });
+        await axios.post(`${config.backend}/api/auth/set-jwt`, user, { withCredentials: true });
     } catch(error) {
         throw new Error("Failed to set JWT cookie");
     }
