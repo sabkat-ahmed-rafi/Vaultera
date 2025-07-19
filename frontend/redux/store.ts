8033eb6c2e1183b7909c46b264fe5fc58b10c9ba
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-19 21:55:08 +0600

    fix: ignore non-serializable data in state for decryptedVaultKey

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+ 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredPaths: ['auth.decryptedVaultKey'],
+          ignoredActionPaths: ['payload'], 
+        },
+      }),
 });
 
 
