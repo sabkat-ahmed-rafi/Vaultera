36b1a903c033f136128657ca412baf6878b2315c
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-19 20:20:42 +0600

    feat: add more field to the global user state

--- frontend/types/User.ts
+++ frontend/types/User.ts
@@ -3,4 +3,13 @@ export interface User {
   email: string | null;
   name: string | null;
   photo: string | null;
+  salt: string | null;
+  iv: string | null;
+  encryptedVaultKey: string | null;
+  paid: string | null;
+  paymentType: string | null;
+  paddleCustomerId: string | null;
+  paddleSubscriptionId: string | null;
+  subscriptionStatus: string | null;
+  paymentExpiresAt: string | null;
 }
--- frontend/utils/mapUser.ts
+++ frontend/utils/mapUser.ts
@@ -5,5 +5,14 @@ export const mapUser = (user: User) => ({
   id: user.id,
   email: user.email,
   name: user.name,
-  photo: user.photo
+  photo: user.photo,
+  salt: user.salt,
+  iv: user.iv,
+  encryptedVaultKey: user.encryptedVaultKey,
+  paid: user.paid,
+  paymentType: user.paymentType,
+  paddleCustomerId: user.paddleCustomerId,
+  paddleSubscriptionId: user.paddleSubscriptionId,
+  subscriptionStatus: user.subscriptionStatus,
+  paymentExpiresAt: user.paymentExpiresAt
 });
