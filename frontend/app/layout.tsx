10b11448e1d31b8cac31c6a28bcf1335bd1745d2
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-21 01:22:42 +0600

    refactor: add AuthLayer to the root layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import "@radix-ui/themes/styles.css";
 
 import ReduxProvider from "@/redux/ReduxProvider";
+import AuthLayer from "@/components/others/AuthLayer";
 
 export const metadata: Metadata = {
   title: "Vaultera",
@@ -17,10 +18,12 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     <html lang="en">
       <body className={`antialiased`}>
         <ReduxProvider>
+            <AuthLayer>
               <Theme appearance="dark" grayColor="sage" accentColor="blue">
                 {children}
                 <Toaster position="bottom-right" />
               </Theme>
+            </AuthLayer>
         </ReduxProvider>
       </body>
     </html>
