0f26d80c1ef2bce9687b32dce8bf6b1630e95e7f
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-07-21 01:25:35 +0600

    refactor: add the master password guard to the home route

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/components/Navbar/Navbar";
+import MasterPasswordGuard from "@/components/others/MasterPasswordGuard";
 
 export default function Home() {
   return (
+    <MasterPasswordGuard>
      <div>
        <Navbar />
      </div>
+    </MasterPasswordGuard>
   );
 }
