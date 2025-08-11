6cf66488b270176c6998133866bb233e2ba90a1c
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-08-11 20:10:06 +0600

    refactor: separate the add password dialog

--- frontend/app/vault/passwords/page.tsx
+++ frontend/app/vault/passwords/page.tsx
@@ -10,6 +10,7 @@ import { RiAddLine, RiShieldKeyholeLine } from "react-icons/ri";
 import PasswordList from "@/components/Passwords/PasswordList";
 import { PasswordItem } from "@/types/Passwords";
 import PasswordEditDialog from "@/components/Passwords/PasswordEditDialog";
+import AddPasswordDialog from "@/components/Passwords/AddPasswordDialog";
 
 
 const fetchPasswords = async (): Promise<PasswordItem[]> => {
@@ -156,55 +157,20 @@ const PasswordsPage: React.FC = () => {
 
         {/* Edit Dialog */}
         <PasswordEditDialog
-        editItem={editItem}
-        setEditItem={setEditItem}
-        editForm={editForm}
-        handleEditChange={handleEditChange}
-        handleEditSave={handleEditSave}
+          editItem={editItem}
+          setEditItem={setEditItem}
+          editForm={editForm}
+          handleEditChange={handleEditChange}
+          handleEditSave={handleEditSave}
         />
 
         {/* Add Dialog */}
-        <Dialog.Root open={addOpen} onOpenChange={setAddOpen}>
-          <Dialog.Content>
-            <Dialog.Title>Add New Password</Dialog.Title>
-            <Flex direction="column" gap="3" mt="3">
-              <TextField.Root
-                value={addForm.name}
-                onChange={(e) => handleAddChange("name", e.target.value)}
-                placeholder="Name">
-
-              </TextField.Root>
-              <TextField.Root
-                value={addForm.username}
-                onChange={(e) => handleAddChange("username", e.target.value)}
-                placeholder="Username">
-              </TextField.Root>
-              <TextField.Root
-                value={addForm.password}
-                onChange={(e) => handleAddChange("password", e.target.value)}
-                placeholder="Password">
-              </TextField.Root>
-              <TextField.Root
-                value={addForm.url ?? ""}
-                onChange={(e) => handleAddChange("url", e.target.value)}
-                placeholder="URL">
-              </TextField.Root>
-              <Flex gap="2" mt="4" justify="end">
-                <Button variant="soft" onClick={() => setAddOpen(false)}>
-                  Cancel
-                </Button>
-                <Button
-                  variant="solid"
-                  color="green"
-                  onClick={handleAddSave}
-                  disabled={!addForm.name || !addForm.username || !addForm.password}
-                >
-                  Add
-                </Button>
-              </Flex>
-            </Flex>
-          </Dialog.Content>
-        </Dialog.Root>
+        <AddPasswordDialog 
+          addOpen={addOpen} 
+          setAddOpen={setAddOpen} 
+          addForm={addForm} 
+          handleAddChange={handleAddChange} 
+          handleAddSave={handleAddSave} />
       </div>
     </>
   );
