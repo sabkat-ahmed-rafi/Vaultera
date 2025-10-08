9ff213206d4b8271f4da4d47e2483ef6db162841
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-10-08 21:35:44 +0600

    feat: add protected route in the vault

--- frontend/app/vault/layout.tsx
+++ frontend/app/vault/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Sidebar from '@/components/Vault/Sidebar/Sidebar';
 import MasterPasswordGuard from "@/components/others/MasterPasswordGuard";
+import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute"
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,12 +10,14 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-black/50 flex">
+     <ProtectedRoute>
       <MasterPasswordGuard>
         <Sidebar />
         <main className="flex-1 min-h-screen overflow-auto">
            {children}
         </main>
       </MasterPasswordGuard>
+     </ProtectedRoute>
     </div>
   );
 }
---
+++ frontend/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react'
+import { useAppSelector } from '@/redux/hooks';
+
+
+
+const PrivateRoute = ( { children }: Readonly<{ children: React.ReactNode }> ) => {
+    
+    const {user, loading} = useAppSelector(state => state.auth);
+    const router = useRouter();
+    
+    useEffect(() => {
+        if (!loading && !user) {
+            router.push('/sign-in');
+          }
+    }, [loading, user, router]);
+
+
+        if(loading) {  
+            return (
+                <div className="flex items-center justify-center h-screen w-screen text-white bg-black text-2xl">
+                  Loading...
+                </div>
+            )
+        }
+
+    
+    
+    if(user) return <>{children}</>
+
+}
+
+export default PrivateRoute
