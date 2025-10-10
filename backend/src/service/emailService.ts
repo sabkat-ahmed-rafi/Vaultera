1859e83b1c4c8513ebbb8c8010fd3b95b67f1249
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-10-10 19:11:30 +0600

    feat: add email service

---
+++ backend/src/service/emailService.ts
@@ -0,0 +1,19 @@
+import { prisma } from "../config/prismaClient";
+import { CreateEmailEntryInput, UpdateEmailEntryInput } from "../types/types";
+
+
+export const listEmailEntries = async (userId: string) => {
+  return prisma.emailEntry.findMany({ where: { userId }, orderBy: { createdAt: "desc" } });
+};
+
+export const createEmailEntry = async (data: CreateEmailEntryInput) => {
+  return prisma.emailEntry.create({ data });
+};
+
+export const updateEmailEntry = async (id: string, userId: string, data: UpdateEmailEntryInput) => {
+  return prisma.emailEntry.updateMany({ where: { id, userId }, data });
+};
+
+export const deletePasswordEntry = async (id: string, userId: string) => {
+  return prisma.emailEntry.deleteMany({ where: { id, userId } });
+};
