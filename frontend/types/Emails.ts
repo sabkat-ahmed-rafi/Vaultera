0089d3357de63b048b710ad736f38edbbf9e0ebd
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-10-10 18:13:17 +0600

    feat: add prop types for email

--- frontend/types/Emails.ts
+++ frontend/types/Emails.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from "react";
 
 export type EmailItem = {
   id: string;
@@ -13,4 +14,20 @@ export interface EmailListProps {
   handleShowPassword: (id: string) => void;
   handleEditClick: (item: EmailItem) => void;
   handleDelete: (id: string) => void;
+}
+
+export interface EmailEditDialogProps {
+  editItem: EmailItem | null;
+  setEditItem: Dispatch<SetStateAction<EmailItem | null>>;
+  editForm: EmailItem | null;
+  handleEditChange: (field: "email" | "username" | "password", value: string) => void;
+  handleEditSave: () => void;
+}
+
+export interface AddEmailDialogProps {
+  addOpen: boolean;
+  setAddOpen: Dispatch<SetStateAction<boolean>>, 
+  addForm: EmailItem, 
+  handleAddChange: (field: "email" | "username" | "password", value: string) => void, 
+  handleAddSave: () => void
 }
