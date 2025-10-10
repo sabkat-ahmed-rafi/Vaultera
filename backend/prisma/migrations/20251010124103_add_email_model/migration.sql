102e1d49b65fc4d09fab02b5dce5898255cbaeaa
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-10-10 18:42:19 +0600

    feat: add new email model

---
+++ backend/prisma/migrations/20251010124103_add_email_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "EmailEntry" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "encryptedSecret" TEXT NOT NULL,
+    "iv" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EmailEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "EmailEntry" ADD CONSTRAINT "EmailEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
