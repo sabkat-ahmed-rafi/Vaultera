272d154e8a83fed174ffdc75f0b3500b848519e6
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-11-02 20:15:16 +0600

    fix: off the no-unused-vars

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,14 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
+  {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_" },
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
     },
-  },
+  }
+
 
 ];
 
