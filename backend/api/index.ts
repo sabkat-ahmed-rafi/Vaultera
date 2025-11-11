6cdf14fe03b7a71f97edbc892206ff8181f33c87
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-11-11 16:11:54 +0600

    refactor: update the index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,2 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import serverless from 'serverless-http';
 import app from '../src/app.js';
-
-const handler = serverless(app);
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  return handler(req as any, res as any);
-}
+export default app;
